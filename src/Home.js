4a11aecd2abe78d821e06a25e4756273a71c1c59
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-04-28 11:57:58 -0700

    add a background gradient to H1

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,11 @@ import { memo } from "react";
 const Home = () => (
 	<div className="lg:grid lg:grid-cols-2 mx-auto max-w-7xl lg:px-8 lg:gap-8 justify-center">
 		<div className="lg: py-24 ">
-			<h1 className="text-white text-4xl mt-4 font-extrabold sm:text-6xl lg:mt-6 tracking-tighty">
+			<h1 className="text-white text-4xl mt-4 font-extrabold sm:text-6xl lg:mt-6 tracking-tight">
 				<span className="block">More than just </span>
-				<span className="block">shorter links</span>
+				<span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-200 to-indigo-400 block">
+					shorter links
+				</span>
 			</h1>
 			<p className="text-white">
 				Build your brand's recognition and get detailed insights on how your
