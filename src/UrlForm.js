ef4e3835fc204b63c038f157c48ed0f7d148d84b
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-04-28 15:45:11 -0700

    Build functional shrtcode api link

--- src/UrlForm.js
+++ src/UrlForm.js
@@ -1,31 +1,77 @@
-import { memo } from "react";
+import { memo, useState } from "react";
+import LoadingIcon from "./LoadingIcon";
 
-const UrlForm = () => (
-	<div className="mt-10 sm:mt-12">
-		<form>
-			<div className="sm:flex">
-				<div classsName="min-w-0 flex-1">
-					<label htmlFor="url" className="sr-only">
-						URL input
-					</label>
-					<input
-						type="text"
-						name="url"
-						placeholder="Place your link here..."
-						className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
-					></input>
-				</div>
-				<div className="mt-3 sm:mt-0 sm:ml-3">
-					<button
-						type="submit"
-						className="block w-full py-3 px-4 h-12 rounded-md shadow bg-gradient-to-r from-indigo-200 to-indigo-400 text-white font-bold hover:from-indigo-600 hover:to-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
-					>
-						Shorten It!
-					</button>
-				</div>
-			</div>
-		</form>
-	</div>
-);
+const UrlForm = () => {
+	const [url, setUrl] = useState("");
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [resultUrl, setResultUrl] = useState({});
+	const shortenUrl = `https://api.shrtco.de/v2/shorten?url=${url}`;
 
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			let response = await fetch(shortenUrl);
+			let result = await response.json();
+			console.log(result);
+			setResultUrl(result);
+			setLoading(false);
+		} catch {
+			setError(true);
+			setLoading(false);
+		}
+	}
+	return (
+		<div className="mt-10 sm:mt-12">
+			<form onSubmit={handleSubmit}>
+				<div className="sm:flex">
+					<div classsName="min-w-0 flex-1">
+						<label htmlFor="url" className="sr-only">
+							URL input
+						</label>
+						<input
+							type="text"
+							name="url"
+							placeholder="Place your link here..."
+							onChange={(e) => {
+								setUrl(e.target.value);
+							}}
+							className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+						></input>
+						{console.log(url)}
+					</div>
+					<div className="mt-3 sm:mt-0 sm:ml-3">
+						<button
+							type="submit"
+							className="block w-full py-3 px-4 h-12 rounded-md shadow bg-gradient-to-r from-indigo-200 to-indigo-400 text-white font-bold hover:from-indigo-600 hover:to-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+						>
+							{loading ? <LoadingIcon /> : "Shorten It!"}
+						</button>
+					</div>
+				</div>
+			</form>
+			{resultUrl.ok ? (
+				<>
+					<div className="mt-5 flex flex-col items-center justify-center rounded-md bg-white py-4">
+						<p className="text-gray-900 sm:mt-4">Link Created</p>
+						<a
+							target="_aboutblank"
+							href={resultUrl.result.full_short_link}
+							className="text-xl mt-2 text-green-500 hover:text-green-600
+						hover:underline"
+						>
+							{resultUrl.result.full_short_link}
+						</a>
+					</div>
+				</>
+			) : (
+				<p className="mt-3 text-sm text-gray-300 sm:mt-4">
+					This website uses shrtcode API and by using it you agree to their
+					Terms of Service
+				</p>
+			)}
+		</div>
+	);
+};
 export default memo(UrlForm);
